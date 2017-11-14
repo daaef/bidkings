b4f72b27acd1e1b511d4264f19071b23ba53c348
Unknown <realafe21@gmail.com>
2017-11-14 17:38:38 +0100

    splash page

--- js/script.js
+++ js/script.js
@@ -1,43 +1,86 @@
-const second = 1000,
-    minute = second * 60,
-    hour = minute * 60,
-    day = hour * 24;
-function pad(n) {
-    return (n < 10 ? '0' : '') + n;
-}
-let countDown = new Date('Mar 1, 2018 00:00:00').getTime(),
-    x = setInterval(function() {
 
-        let now = new Date().getTime(),
-            distance = countDown - now;
-            document.getElementById('hours').innerHTML = pad(Math.floor((distance % (day)) / (hour))),
-            document.getElementById('minutes').innerHTML = pad(Math.floor((distance % (hour)) / (minute))),
-            document.getElementById('seconds').innerHTML = pad(Math.floor((distance % (minute)) / second));
+$(window).on('load', function () {
+    let labels = ['weeks', 'days', 'hrs', 'minutes', 'seconds']
+        , nextYear = '2017/12/25'
+        , template = _.template($('#main-example-template').html())
+        , currDate = '00:00:00:00:00'
+        , nextDate = '00:00:00:00:00'
+        , parser = /([0-9]{2})/gi
+        , $example = $('#main-example');
 
-        //do something later when date is reached
-        if (distance < 0) {
-            clearInterval(x);
+    // Parse countdown string to an object
+    function strfobj(str) {
+        var parsed = str.match(parser)
+            , obj = {};
+        labels.forEach(function (label, i) {
+            obj[label] = parsed[i]
+        });
+        return obj;
+    }
+    // Return the time components that diffs
+    function diff(obj1, obj2) {
+        var diff = [];
+        labels.forEach(function (key) {
+            if (obj1[key] !== obj2[key]) {
+                diff.push(key);
+            }
+        });
+        return diff;
+    }
+    // Build the layout
+    var initData = strfobj(currDate);
+    labels.forEach(function (label, i) {
+        $example.append(template({
+            curr: initData[label]
+            , next: initData[label]
+            , label: label
+        }));
+    });
+    // Starts the countdown
+    $example.countdown(nextYear, function (event) {
+        var newDate = event.strftime('%w:%d:%H:%M:%S')
+            , data;
+        if (newDate !== nextDate) {
+            currDate = nextDate;
+            nextDate = newDate;
+            // Setup the data
+            data = {
+                'curr': strfobj(currDate)
+                , 'next': strfobj(nextDate)
+            };
+            // Apply the new values to each node that changed
+            diff(data.curr, data.next).forEach(function (label) {
+                var selector = '.%s'.replace(/%s/, label)
+                    , $node = $example.find(selector);
+                // Update the node
+                $node.removeClass('flip');
+                $node.find('.curr').text(data.curr[label]);
+                $node.find('.next').text(data.next[label]);
+                // Wait for a repaint to then flip
+                _.delay(function ($node) {
+                    $node.addClass('flip');
+                }, 50, $node);
+            });
         }
+    });
 
-    }, second);
+    var timer2 = $('[count]').attr('count');
+    var interval = setInterval(function() {
 
-function call(){
-    clearInterval(x);
-    countDown = new Date().setSeconds(new Date().getSeconds() + 16);
-    return x = setInterval(function() {
 
-        let now = new Date().getTime(),
-            distance = countDown - now;
-        document.getElementById('hours').innerHTML = pad(Math.floor((distance % (day)) / (hour))),
-            document.getElementById('minutes').innerHTML = pad(Math.floor((distance % (hour)) / (minute))),
-            document.getElementById('seconds').innerHTML = pad(Math.floor((distance % (minute)) / second));
-
-        if (distance < 1000) {
-//  countDown = new Date().setSeconds(new Date().getSeconds() + 16);
-            clearInterval(x);
-            document.querySelector('. uk-padding-remove').innerHTML = "<h1>SOLD</h1>"
-
-        }
-
-    }, second);
-}
+        var timer = timer2.split(':');
+        //by parsing integer, I avoid all extra string processing
+        var hours = parseInt(timer[0], 10);
+        var minutes = parseInt(timer[1], 10);
+        var seconds = parseInt(timer[2], 10);
+        --seconds;
+        hours = (minutes < 0) ? --hours : hours;
+        minutes = (seconds < 0) ? --minutes : minutes;
+        if (hours < 1 && minutes < 1 && seconds < 1) clearInterval(interval);
+        seconds = (seconds < 0) ? 59 : seconds;
+        seconds = `${(seconds < 10) ? '0' + seconds : seconds}`;
+        //minutes = (minutes < 10) ?  minutes : minutes;
+        $('.countdown').html(`${hours}:${minutes}:${seconds}`);
+        timer2 =`${hours}:${minutes}:${seconds}`;
+    }, 1000);
+});
